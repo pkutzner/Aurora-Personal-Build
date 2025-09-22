dc0d91c37f5716bef9ec543e2cd6c09e5d47d1e3
Preston Kutzner <pkutzner@gmail.com>
2025-09-22 12:52:59 -0500

    Add build packages for VMWare kernel modules

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux aria2.x86_64
+dnf5 install -y tmux aria2.x86_64 kernel-devel kernel-headers gcc make patch wget git
 
 # Use a COPR Example:
 #
